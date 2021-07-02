5aaad2e20ad703e3eb56df35bafc35c2e9fc8f50
NerDante <liuyongqiangx@hotmail.com>
2021-07-02 23:59:35 +0800

    Modify cc-mode indent things

--- lorq/lorq-prog.el
+++ lorq/lorq-prog.el
@@ -5,13 +5,41 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;   C/C++ ;;;;;;;;;;;;;;;;;;;;;;
 
-;; c indent style
-(setq c-default-style "linux"
-      c-basic-offset 4)
+;;My C Programming Style
+(defconst lorq/c-style
+  '((c-tab-always-indent . t)
+    (c-comment-only-line-offset . 0)
+    (c-hanging-braces-alist . ((substatement-open after)
+                               (brace-list-open)))
+    (c-hanging-colons-alist . ((member-init-intro before)
+                               (inher-intro)
+                               (case-label after)
+                               (label after)
+                               (access-label after)))
+    (c-cleanup-list . (scope-operator
+                       empty-defun-braces
+                       defun-close-semi))
+    (c-offsets-alist . ((arglist-close . c-lineup-arglist)
+                        (substatement-open . 0)
+                        (case-label . 4)
+                        (block-open . 0)
+                        (knr-argdecl-intro . -)))
+    (c-echo-syntactic-information-p . t)
+    ))
+
+;; offset customizations not in lorq/c-style
+(setq c-offsets-alist '((member-init-intro . ++)))
+
+(defun lorq/c-mode-common-hook ()
+  (c-add-style "PERSONAL" lorq/c-style t)
+  (setq tab-width 4
+        indent-tabs-mode nil)
+  (define-key c-mode-base-map "\C-m" 'c-context-line-break))
+
+(add-hook 'c-mode-common-hook 'lorq/c-mode-common-hook)
 
 (use-package cc-mode)
 (use-package cmake-mode)
-
 (use-package modern-cpp-font-lock
   :ensure t
   :config
